59f2ae25586c63bc7619da92a7dc437417741dab
董红豆 <2514184696@qq.com>
2023-08-03 18:27:05 +0800

    ！

---
+++ back/src/main/java/com/web/back/utils/bean/BeanValidators.java
@@ -0,0 +1,23 @@
+package com.web.back.utils.bean;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.util.Set;
+
+/**
+ * @author by hongdou
+ * @date 2023/8/3.
+ * @DESC:
+ */
+public class BeanValidators {
+    public static void validateWithException(Validator validator, Object object, Class<?>... groups)
+            throws ConstraintViolationException
+    {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(object, groups);
+        if (!constraintViolations.isEmpty())
+        {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
+}
